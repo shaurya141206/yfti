cbbd3c0fee41f41437e577e76cb26d8793116e89
shaurya141206 <66153195+shaurya141206@users.noreply.github.com>
2020-06-09 20:47:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+//C36 PAINTING CANVAS
+//JS HARRISON
+var canvas;
+var database;
+var isDrawing = false;
+var currentPath = [];
+var drawing = [];
+var dB_drawing = [];
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    //    background("yellow")
+    canvas.parent("canvascontainer");
+    database = firebase.database();
+
+    canvas.mousePressed(startPath);
+    canvas.mouseReleased(endPath);  
+}
+
+function startPath() {
+    isDrawing = true;
+    currentPath = [];
+    drawing.push(currentPath);
+}
+
+function endPath() {
+    isDrawing = false;
+    //dBref.push(drawing);
+}
+
+function draw() {
+    background("cyan");
+    if (isDrawing) {
+        var point = {
+            x: mouseX,
+            y: mouseY
+        };
+        currentPath.push(point);
+    }
+    stroke(25, 34, 234);
+    strokeWeight(4);
+    noFill();
+    for (var i = 0; i < drawing.length; i++) {
+        var path = drawing[i];
+        beginShape();
+        for (var j = 0; j < path.length; j++) {
+            vertex(path[j].x, path[j].y);
+        }
+        endShape();
+    }
+
+    //Reading from database
+    readData();
+    stroke(255, 0, 0);
+    strokeWeight(4);
+    noFill();
+
+    for (var i = 0; i < dB_drawing.length; i++) {
+         var path = dB_drawing[i];
+         beginShape();
+         for (var j = 0; j < path.length; j++) {
+             vertex(path[j].x, path[j].y);
+         }
+         endShape();
+    }
+}
+
+function readData() {
+    database.ref('MonaLisa/Session1/drawing/').on('child_added', function(data) {
+        dB_drawing.push(data.val());
+        console.log(dB_drawing);
+    })
+}
